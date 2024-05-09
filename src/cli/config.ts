ade3f5a75f964140c4215d972dc34ddca062e7b0
Bohdan Shulha <b.shulha@pm.me>
2024-05-09 11:28:16 +0200

    feat #4: export individual config branches

--- src/cli/config.ts
+++ src/cli/config.ts
@@ -1,26 +1,41 @@
 import { z } from "zod";
 
 export const DualshockConfig = z.object({
-	schema: z.object({
-		registry: z.record(
-			z.object({
-				args: z.any(),
-				returns: z.any(),
-			}),
-		),
-		events: z.record(
-			z.object({
-				payload: z.any(),
-			}),
-		),
-		output: z.string().default("dualshock.schema.json"),
-	}),
-	typescript: z.object({
-		schema: z.string().default("dualshock.schema.json"),
-		output: z.string().default("src/dualshock.gen.ts"),
-		rpcTypeName: z.string().default("DualshockInvokables"),
-		eventTypeName: z.string().default("DualshockEvents"),
-	}),
+	schema: z
+		.object({
+			registry: z
+				.record(
+					z.object({
+						args: z.any(),
+						returns: z.any(),
+					}),
+				)
+				.default({}),
+			events: z
+				.record(
+					z.object({
+						payload: z.any(),
+					}),
+				)
+				.default({}),
+			output: z.string().default("dualshock.schema.json"),
+		})
+		.default({}),
+	typescript: z
+		.object({
+			schema: z.string().default("dualshock.schema.json"),
+			output: z.string().default("src/dualshock.gen.ts"),
+			rpcTypeName: z.string().default("DualshockInvokables"),
+			eventTypeName: z.string().default("DualshockEvents"),
+		})
+		.default({}),
 });
 
 export type DualshockConfig = z.infer<typeof DualshockConfig>;
+export type DualshockSchemaConfig = {
+	schema: DualshockConfig["schema"];
+};
+
+export type DualshockTypescriptConfig = {
+	typescript: DualshockConfig["typescript"];
+};
