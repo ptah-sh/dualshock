c8f42ab3e2ecc67842abe6ff8f2078bb1e5ba210
Bohdan Shulha <b.shulha@pm.me>
2024-05-09 11:05:35 +0200

    feat #4: re-structure dualshock config processing

--- src/cli/config.ts
+++ src/cli/config.ts
@@ -1,18 +1,26 @@
 import { z } from "zod";
 
 export const DualshockConfig = z.object({
-	registry: z.record(
-		z.object({
-			args: z.any(),
-			returns: z.any(),
-		}),
-	),
-	events: z.record(
-		z.object({
-			payload: z.any(),
-		}),
-	),
-	output: z.string().default("dualshock.schema.json"),
+	schema: z.object({
+		registry: z.record(
+			z.object({
+				args: z.any(),
+				returns: z.any(),
+			}),
+		),
+		events: z.record(
+			z.object({
+				payload: z.any(),
+			}),
+		),
+		output: z.string().default("dualshock.schema.json"),
+	}),
+	typescript: z.object({
+		schema: z.string().default("dualshock.schema.json"),
+		output: z.string().default("src/dualshock.gen.ts"),
+		rpcTypeName: z.string().default("DualshockInvokables"),
+		eventTypeName: z.string().default("DualshockEvents"),
+	}),
 });
 
 export type DualshockConfig = z.infer<typeof DualshockConfig>;
