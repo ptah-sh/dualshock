3b05c3f18d039162315851440252763626a4acba
Bohdan Shulha <b.shulha@pm.me>
2024-05-14 12:31:16 +0200

    feat #7: export RpcConnection for dualshock-react

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { RpcServer } from "./RpcServer.js";
 export { RpcClient } from "./RpcClient.js";
+export { RpcConnection } from "./RpcConnection.js";
 export { WebSocketDom } from "./websocket/WebSocket.dom.js";
 export { InternalError, ValidationError } from "./errors.js";
 export { type TRefine as Refine, refine } from "./refine.js";
